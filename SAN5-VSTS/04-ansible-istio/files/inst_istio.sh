e8d838f1aaa08d8e014e672cf580e5d286ba82b8
root <renan.cardoso93@hotmail.com>
2020-11-16 18:28:58 -0300

    delete

--- SAN5-VSTS/04-ansible-istio/files/inst_istio.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Specify the Istio version that will be leveraged throughout these instructions
-ISTIO_VERSION=1.4.0
-cd /tmp
-curl -sL "https://github.com/istio/istio/releases/download/$ISTIO_VERSION/istio-$ISTIO_VERSION-linux.tar.gz" | tar xz
-cp istio-1.4.0/bin/istioctl /usr/local/bin/
